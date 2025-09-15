f51f6a6c37619abc0f863c83a458ba1b0ef75d2b
Aleah P. <92504607+AleahPorras@users.noreply.github.com>
2025-09-14 20:54:39 -0700

    Updated a-star.py
    Finished parsing the text files into dictionaries.

--- a-star.py
+++ a-star.py
@@ -4,19 +4,20 @@ import argparse
 ###------------------------------------------------------###
 # code for extra arguments on command line
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('Start', type = str, required = True, help = "Name of departing city.")
-# parser.add_argument('End', type = str, required = True, help = "Name of arriving city.")
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("Start", type = str, required = True, help = "Name of departing city.")
+parser.add_argument("End", type = str, required = True, help = "Name of arriving city.")
+args = parser.parse_args()
 
-# # opening files
+starting_city = args.start 
+ending_city = args.end
 
-city_coordinates = open("coordinates.txt", "r")
-city_distances = open("map.txt", "r")
+city_coordinates = "coordinates.txt"
+city_distances = "map.txt"
 
 ###------------------------------------------------------###
 ## Parsing in text from coordinates.txt
-# Status: Not Started
+# Status: Done?
 
 def read_coordinates(file):
     dictionary = {}
@@ -25,8 +26,8 @@ def read_coordinates(file):
         # looks through entire file
         for line in f:
 
-            key, value = line.strip().split(':')
-            lat, lon = value.strip("()").split(',')
+            key, value = line.strip().split(":")
+            lat, lon = value.strip("()").split(",")
             dictionary[key] = (float(lat), float(lon))
 
     # should return dict, {city: (latitude, longitude)}
@@ -35,11 +36,23 @@ def read_coordinates(file):
 
 ###------------------------------------------------------###
 ## Parsing in text from map.txt
-# Status: Not Started
+# Status: Done?
 
 def parse_map(file):
+    dictionary = {}
 
+    with open(file, "r") as f:
+        for line in f:
+            og_city, value = line.strip().split("-")
+            dictionary[og_city] = {}
+
+            for i in value.split(","):
+                neighbor_city, distance = i.split("(")
+                distance = float(distance.strip(")"))
+                dictionary[og_city][neighbor_city] = distance
+                
     # should return dict, {city: {neighbor:distance}}
+    return dictionary
     pass
 
 ###------------------------------------------------------###
@@ -58,4 +71,5 @@ def haversine_formula(start, end):
 ## Actual A* algorithm, will find the optimal path to the destination city
 # Status: Not Started
 
-print(read_coordinates("coordinates.txt"))
+# print(read_coordinates(city_coordinates))
+print(parse_map(city_distances))
